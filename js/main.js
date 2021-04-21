c1cbb208f2960b071b2b50d384a149e7f6dd4b35
b3q <darrenb3q@byui.edu>
2021-04-20 20:20:29 -0600

    20210420_2020

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const links = [
+    {
+        label: "Week01 Notes",
+        url: "week01/index.html"
+    },
+    {
+        label: "Week02 Notes",
+        url: "week02/index.html"
+    }
+]
+
+for(let i = 0; i < links.length; i++) {
+    let label = document.createElement('li');
+    let link = document.createElement('a');
+    link.setAttribute('href', links[i].url);
+    link.textContent = links[i].label;
+    // label.textContent = links[i].label;
+
+    label.appendChild(link);
+
+    document.querySelector('ol#weeksol').appendChild(label);
+}
