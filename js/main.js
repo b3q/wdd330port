d4642fdd96d3b037f9757082cb23948e6a4dee24
b3q <darrenb3q@byui.edu>
2021-05-08 21:34:21 -0600

    20210508_2134

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ const links = [
     {
         label: "Week03 Notes",
         url: "week03/index.html"
+    },
+    {
+        label: "Week04 Notes",
+        url: "week04/index.html"
     }
 ]
 
---
+++ week04/main.js
@@ -0,0 +1,23 @@
+// const form = document.forms['search'];
+
+// input.addEventListener('focus', () => alert('focused'), false);
+// input.addEventListener('blur', () => alert('blurred'), false);
+// input.addEventListener('change', () => alert('changed'), false);
+
+const form = document.forms['search'];
+const input = form.elements.searchInput;
+form.addEventListener ('submit', search, false);
+function search(event) {
+    alert(`You search for: ${input.value}`);
+    event.preventDefault();
+}
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
